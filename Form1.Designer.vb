0ee34328f47c68b1c3407c2c96717e24390dad29
Liam Rivers <129525070+lriv42@users.noreply.github.com>
2024-01-14 12:10:29 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstRates = New System.Windows.Forms.ListBox()
+        Me.lstYears = New System.Windows.Forms.ListBox()
+        Me.txtAmtFinanced = New System.Windows.Forms.TextBox()
+        Me.lblMonthPayment = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lstRates
+        '
+        Me.lstRates.FormattingEnabled = True
+        Me.lstRates.ItemHeight = 16
+        Me.lstRates.Location = New System.Drawing.Point(255, 52)
+        Me.lstRates.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.lstRates.Name = "lstRates"
+        Me.lstRates.Size = New System.Drawing.Size(167, 196)
+        Me.lstRates.TabIndex = 1
+        '
+        'lstYears
+        '
+        Me.lstYears.FormattingEnabled = True
+        Me.lstYears.ItemHeight = 16
+        Me.lstYears.Location = New System.Drawing.Point(481, 52)
+        Me.lstYears.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.lstYears.Name = "lstYears"
+        Me.lstYears.Size = New System.Drawing.Size(148, 196)
+        Me.lstYears.TabIndex = 2
+        '
+        'txtAmtFinanced
+        '
+        Me.txtAmtFinanced.Location = New System.Drawing.Point(80, 52)
+        Me.txtAmtFinanced.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtAmtFinanced.Name = "txtAmtFinanced"
+        Me.txtAmtFinanced.Size = New System.Drawing.Size(131, 22)
+        Me.txtAmtFinanced.TabIndex = 0
+        '
+        'lblMonthPayment
+        '
+        Me.lblMonthPayment.AutoSize = True
+        Me.lblMonthPayment.BackColor = System.Drawing.SystemColors.Info
+        Me.lblMonthPayment.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMonthPayment.Location = New System.Drawing.Point(251, 330)
+        Me.lblMonthPayment.MinimumSize = New System.Drawing.Size(387, 62)
+        Me.lblMonthPayment.Name = "lblMonthPayment"
+        Me.lblMonthPayment.Size = New System.Drawing.Size(387, 62)
+        Me.lblMonthPayment.TabIndex = 5
+        '
+        'btnCalc
+        '
+        Me.btnCalc.BackColor = System.Drawing.SystemColors.Info
+        Me.btnCalc.Font = New System.Drawing.Font("Microsoft Tai Le", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalc.Location = New System.Drawing.Point(80, 116)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(108, 47)
+        Me.btnCalc.TabIndex = 3
+        Me.btnCalc.Text = "C&alculate"
+        Me.btnCalc.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Firebrick
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Tai Le", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnExit.Location = New System.Drawing.Point(80, 202)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(108, 47)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Ex&it"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(76, 33)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(130, 17)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Principle Amount"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(75, 332)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(159, 58)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Your Monthly " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Payment is:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(252, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 34)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Interest Rate " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Percentage"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(481, 29)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(157, 17)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Year Length of Loan"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblMonthPayment)
+        Me.Controls.Add(Me.txtAmtFinanced)
+        Me.Controls.Add(Me.lstYears)
+        Me.Controls.Add(Me.lstRates)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Loan Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstRates As ListBox
+    Friend WithEvents lstYears As ListBox
+    Friend WithEvents txtAmtFinanced As TextBox
+    Friend WithEvents lblMonthPayment As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+        'this closes the form
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        For decRates As Decimal = 5 To 8 Step 0.25
+            lstRates.Items.Add(decRates)
+        Next decRates
+
+        For decYears As Decimal = 1 To 30
+            lstYears.Items.Add(decYears)
+        Next decYears
+
+        'this provides the list of numbers in the list boxes without me having to type them each out individually
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim decYears As Decimal ' multiply by 12 to get months
+        Dim decRates As Decimal ' divide by 12 
+        Dim decAmtFinanced As Decimal ' the principle
+        Dim decMonthPayment As Decimal ' final total to be in label
+
+        'my variables declared
+
+        Decimal.TryParse(lstRates.SelectedItem.ToString, decRates)
+        Decimal.TryParse(lstYears.SelectedItem.ToString, decYears)
+        Decimal.TryParse(txtAmtFinanced.Text, decAmtFinanced)
+
+        'this assigns the selected value from the listboxes and text box to the appropriate variables
+
+        If decAmtFinanced < 100 Then
+            MsgBox("Please enter a valid number greater than 100")
+            txtAmtFinanced.Text = String.Empty
+            lblMonthPayment.Text = String.Empty
+        End If
+
+        'this makes sure a number greater than 100 is entered, if not a message pops up and clears the textbox and label
+
+        'Equation rate/12, nPer*12, PV
+
+        decRates /= 100
+        'this makes the percentage into a decimal so that it works in the calculations
+
+        decMonthPayment = -Financial.Pmt(decRates / 12, decYears * 12, decAmtFinanced)
+
+
+        lblMonthPayment.Text = decMonthPayment.ToString("C2")
+
+        'This is the equation for calculating the monthly payment. It then puts the total into the label
+
+        'I realized the error on my part. I was doing the calculation wrong when trying to check it against the calculator.
+        'when you initially said to use the financial payment method it was actually correct so I apologize for the mishap on my part and hopefully got it squared away.
+
+    End Sub
+
+
+    Private Sub lstRates_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstRates.SelectedIndexChanged
+        lblMonthPayment.Text = String.Empty
+        'this clears the label if the selection is changed
+    End Sub
+
+    Private Sub lstYears_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstYears.SelectedIndexChanged
+        lblMonthPayment.Text = String.Empty
+        'this clears the label if the selection is changed
+    End Sub
+
+    Private Sub txtAmtFinanced_TextChanged(sender As Object, e As EventArgs) Handles txtAmtFinanced.TextChanged
+        lblMonthPayment.Text = String.Empty
+        'this clears the label if the number is changed
+    End Sub
+
+    Private Sub txtAmtFinanced_Click(sender As Object, e As EventArgs) Handles txtAmtFinanced.Click
+        txtAmtFinanced.SelectAll()
+        'This selects all the text so the user can type over it when they re-enter the textbox
+    End Sub
+End Class
